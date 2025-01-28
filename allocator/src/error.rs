e4939e69215d379f6bff6da58617b4d52ecd8ce3
eraykaradag <eraykrdg1@gmail.com>
2025-01-28 13:04:48 +0000

    new allocator service

---
+++ allocator/src/error.rs
@@ -0,0 +1,18 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error
+{
+	#[error(transparent)]
+	ParseInt(#[from] std::num::ParseIntError),
+	#[error(transparent)]
+	TryFromInt(#[from] std::num::TryFromIntError),
+	#[error(transparent)]
+	Allocation(#[from] super::resources::Error),
+	#[error("Nitro CLI error: {0}")]
+	NitroCli(String),
+	#[error("Config file cannot include cpu_count and cpu_pool tag at the same time")]
+	BothOptionsForCpu,
+	#[error("Invalid config file. This might happened due to old config file or config file corruption. See release notes :")]
+	ConfigFileCorruption,
+	#[error("WARNING! Requested resource pool is more than supported. Supported Enclave number is 4")]
+	MoreResourcePoolThanSupported,
+}
